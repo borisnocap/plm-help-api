3433a660c7b2b2c32f477d2022ccdae4bf3fc211
PC <@pc>
2024-05-27 08:39:35 +0300

    Новый вариант регистрации справки по команде. Подключен BukkitAPI.

--- src/main/java/ru/plm/help/HelpAPI.java
+++ src/main/java/ru/plm/help/HelpAPI.java
@@ -1,6 +1,8 @@
 package ru.plm.help;
 
+import net.kyori.adventure.text.TextComponent;
+
 public interface HelpAPI {
 
-    void registerCommandHelp(String commandName, StringBuilder commandHelp);
+    void registerCommandHelp(String command, String permission, TextComponent specialDescription, String... usage);
 }
